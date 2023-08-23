571f9ebbe03058ca4503183401314ce0cf46f084
MaximilianoTRZ <eldiezcele@gmail.com>
2023-08-22 22:04:43 -0300

    struct User, mapping users and functions refactorized

--- SimpleBank.sol
+++ SimpleBank.sol
@@ -8,16 +8,25 @@ contract SimpleBank {
 
     /* Rellenar con la palabra clave de visibilidad. 
     Pista: Queremos proteger el saldo de nuestros usuarios de otros contratos */
-    mapping(address => uint) private balances;
+    // mapping(address => uint) private balances;
 
     /* Rellenar con la palabra clave de visibilidad. 
     Queremos crear una función getter y permitir 
     que los contratos vean si un usuario está inscrito.  */
-    mapping(address => bool) public enrolled;
+    // mapping(address => bool) public enrolled;
 
     /* Asegurémonos de que todos sepan quién es el dueño del banco. 
     Usa la palabra clave de visibilidad adecuada para esto */
     address public owner;
+
+    //BONUS 2
+    struct User{
+        address userId;
+        bool enrolled;
+        uint balance;
+    }
+    // Mapping which index address and its UserStruct 
+    mapping(address => User) private users;
 
     // 
     // Eventos
@@ -42,12 +51,12 @@ contract SimpleBank {
     // Modifiers
     //
     modifier erolledUser {
-        require(enrolled[msg.sender], "User not enrolled");
+        require(users[msg.sender].enrolled, "User not enrolled v222222");
         _; 
     }
 
-    modifier amountGreaterThanZero (uint256 _amount) {
-        require(_amount > 0, "The amount must be greater than 0");
+    modifier greaterThanZeroAmount (uint256 _amount) {
+        require(_amount > 0, "The amount must be greater than v222222");
         _;
     }
 
@@ -74,18 +83,22 @@ contract SimpleBank {
     /// @return El saldo del usuario
     function getBalance() external view returns (uint) {
         /* Obtiene el saldo del remitente de esta transacción */
-        return balances[msg.sender];
+        return users[msg.sender].balance;
     }
 
     /// @notice Inscribir a un cliente en el banco
     /// @return El estado de inscripción del usuario
     // Emite el evento apropiado
     function enroll() public returns (bool) {
-        require(!enrolled[msg.sender], "User already enrolled");
+        require(!users[msg.sender].enrolled, "User already enrolled v222222");
         // ...
-        enrolled[msg.sender]= true;
+        users[msg.sender] = User({
+            userId: msg.sender,
+            enrolled: true,
+            balance: 0
+        });
         emit LogEnrolled(msg.sender);
-        return enrolled[msg.sender];
+        return users[msg.sender].enrolled;
     }
 
     /// @notice Depositar ether en el banco
@@ -96,13 +109,13 @@ contract SimpleBank {
     // Agrega la cantidad al saldo del usuario
     // Emite el evento apropiado
     // devuelve el saldo del usuario
-    function deposit() public payable erolledUser amountGreaterThanZero(msg.value) returns (uint) {
+    function deposit() public payable erolledUser greaterThanZeroAmount(msg.value) returns (uint) {
         // require(enrolled[msg.sender], "User not enrolled");
         // ...
         // require(msg.value > 0, "Deposit amount must be greater than 0");
-        balances[msg.sender] += msg.value;
+        users[msg.sender].balance += msg.value;
         emit LogDepositMade(msg.sender, msg.value);
-        return balances[msg.sender];
+        return users[msg.sender].balance;
     }
 
     /// @notice Retirar ether del banco
@@ -112,14 +125,16 @@ contract SimpleBank {
     // Si el saldo del remitente es al menos la cantidad que quiere retirar
     // intenta enviar esa cantidad de ether al usuario que intenta retirar. 
     // Emite el evento apropiado
-    function withdraw(uint withdrawAmount) public erolledUser amountGreaterThanZero(withdrawAmount) returns (uint) {
-        // require(enrolled[msg.sender] , "User not enrolled");
-        // ...
-        require(balances[msg.sender] >= withdrawAmount, "Insufficient user funds");
-        balances[msg.sender] -= withdrawAmount;
+    function withdraw(uint withdrawAmount) public erolledUser greaterThanZeroAmount(withdrawAmount) returns (uint) {
+        require(users[msg.sender].balance >= withdrawAmount, "Insufficient user funds");
+        users[msg.sender].balance -= withdrawAmount;
         (bool success, ) = msg.sender.call{value: withdrawAmount}("");
         require(success, "Failed to withdraw Ether");
-        emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
-        return balances[msg.sender];
+        emit LogWithdrawal(msg.sender, withdrawAmount, users[msg.sender].balance);
+        return users[msg.sender].balance;
+    }
+
+    function enrolled(address user) public view returns (bool) { 
+        return users[user].enrolled;
     }
 }
